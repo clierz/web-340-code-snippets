7dea91aa4edfae556ccd79f05d7b24313b7b6b61
Richard Krasso <rkrasso@bellevue.edu>
2017-12-03 21:31:58 -0600

    Adding week 1 exercises and assignments.

---
+++ week-1/hello-world.js
@@ -0,0 +1,18 @@
+var http = require("http");
+
+function processRequest(req, res) {
+    var body = "Hello World";
+
+    var contentLength = body.length;
+
+    res.writeHead(200, {
+        'Content-Length': contentLength,
+        'Content-Type': 'text/plain'
+    });
+
+    res.end(body);
+}
+
+var s = http.createServer(processRequest);
+
+s.listen(8080);
