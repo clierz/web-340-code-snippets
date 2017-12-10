2cc73ed2d0198d5b266b01212ca5b47288b10882
Richard Krasso <rkrasso@bellevue.edu>
2017-12-09 18:14:33 -0600

    Adding week four exercises and assignments.

---
+++ week-4/json-api/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var http = require("http");
+
+var app = express();
+
+app.get("/customer/:id", function (request, response) {
+    var id = parseInt(request.params.id, 10);
+
+    response.json({
+        firstName: "Leo",
+        lastName: "Tolstoy",
+        employeeId: id
+    });
+});
+
+http.createServer(app).listen(8080, function() {
+    console.log("Application started on port 8080");
+});
